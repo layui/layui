f6f7411f919ffe57059a5afe86a46bb0bcf14fb0
xuexb <fe.xiaowu@gmail.com>
2017-08-23 13:55:23 +0800

    修改测试配置

--- karma.conf.unit.js
+++ karma.conf.unit.js
@@ -3,12 +3,10 @@
  * @author fe.xiaowu@gmail.com
  */
 
-const base = require('./karma.conf.base.js');
+var base = require('./karma.conf.base.js');
 
 module.exports = function(config) {
-    const options = Object.assign(base(config), {
-
-    });
+    var options = Object.assign(base(config), {});
 
     config.set(options);
 };
