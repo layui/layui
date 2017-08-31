76413b8e7b2a379db9c5f4436fe7e15a7532c729
xuexb <fe.xiaowu@gmail.com>
2017-08-31 23:27:48 +0800

    添加浏览器测试

--- karma.conf.sauce.js
+++ karma.conf.sauce.js
@@ -84,7 +84,7 @@ module.exports = function (config) {
     var options = Object.assign(base(config), {
         reporters: ['mocha', 'saucelabs'],
         sauceLabs: {
-            'testName': 'layui test case',
+            'testName': 'layui',
             'recordVideo': false,
             'recordScreenshots': false,
             'startConnect': false,
@@ -92,7 +92,7 @@ module.exports = function (config) {
                 'no-ssl-bump-domains': 'all'
             },
             'public': 'public',
-            'build': process.env.CIRCLE_BUILD_NUM || process.env.SAUCE_BUILD_ID || 'build-' + Date.now(),
+            'build': 'layui-build-' + process.env.TRAVIS_BUILD_NUMBER,
             'tunnelIdentifier': process.env.TRAVIS_JOB_NUMBER
         },
         customLaunchers: customLaunchers,
