041c812dc4506e013ad792ff01b32d04f2ed537d
xuexb <fe.xiaowu@gmail.com>
2017-11-19 11:26:56 +0800

    chore: 修改浏览器配置

--- karma.conf.sauce.js
+++ karma.conf.sauce.js
@@ -13,16 +13,16 @@ var customLaunchers = {
     },
 
     // 安卓浏览器
-    sl_android_4_4: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        version: '4.4'
-    },
-    sl_android_5: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        version: '5'
-    },
+    // sl_android_4_4: {
+    //     base: 'SauceLabs',
+    //     browserName: 'android',
+    //     version: '4.4'
+    // },
+    // sl_android_5: {
+    //     base: 'SauceLabs',
+    //     browserName: 'android',
+    //     version: '5'
+    // },
     sl_android_6: {
         base: 'SauceLabs',
         browserName: 'android',
@@ -43,33 +43,28 @@ var customLaunchers = {
     sl_ie_9: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 7',
         version: '9'
     },
     sl_ie_10: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 8',
         version: '10'
     },
     sl_ie_11: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 8.1',
         version: '11'
     },
 
     sl_edga: {
         base: 'SauceLabs',
         browserName: 'microsoftedge',
-        platform: 'Windows 10',
-        version: '15'
+        // platform: 'Windows 10'
     },
 
     sl_firefox: {
         base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'Windows 7'
+        browserName: 'firefox'
     }
 };
 
