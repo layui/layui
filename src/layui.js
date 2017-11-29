68bfc4c013a794d4c6e8fa67bc73c1b586ca54b0
sentsin <xu@sentsin.com>
2017-11-29 22:03:15 +0800

    修正注释

--- src/layui.js
+++ src/layui.js
@@ -158,9 +158,9 @@
       var node = doc.createElement('script')
       
       //如果是内置模块，则按照 dir 参数拼接模块路径
-      //如果是扩展模块，则判断模块路径值是否为 {!} 开头，
-      //  如果路径值是 {/} 开头，则模块路径即为后面紧跟的字符。
-      //  否则，则按照 base 参数拼接模块路径
+      //如果是扩展模块，则判断模块路径值是否为 {/} 开头，
+      //如果路径值是 {/} 开头，则模块路径即为后面紧跟的字符。
+      //否则，则按照 base 参数拼接模块路径
       ,url = ( modules[item] ? (dir + 'lay/') 
         : (/^\{\/\}/.test(that.modules[item]) ? '' : (config.base || ''))
       ) + (that.modules[item] || item) + '.js';
