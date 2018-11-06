0563685d63e24d2aeb09162a32ad446655cf7556
xiaohui.lam <xiaohui.lam@icloud.com>
2018-11-07 01:03:16 +0800

    initial laravel service provider

---
+++ laravel/LayuiServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Layui;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Layui的laravel服务包
+ */
+class LayuiServiceProvider extends ServiceProvider
+{
+    /**
+     * 发布Layui的`dist`到public目录下
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            'dist/' => public_path('vendor/layui/'),
+        ], 'layui');
+    }
+}
