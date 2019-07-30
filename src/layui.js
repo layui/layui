c0af8e851ce853de155a5b11ea2b5fa6405f243d
wenxiaofang <858874032@qq.com>
2019-07-30 15:47:20 +0800

    layui table 排序 有负数的时候，由大到小，负数在0上面；由小到大，负数在0下面

--- src/layui.js
+++ src/layui.js
@@ -462,21 +462,11 @@
     
     //如果是数字，按大小排序，如果是非数字，按字典序排序
     clone.sort(function(o1, o2){
-      var isNum = /^-?\d+$/
-      ,v1 = o1[key]
-      ,v2 = o2[key];
+      var v1 = o1[key],v2 = o2[key];
       
-      // if(isNum.test(v1)) v1 = parseFloat(v1);
-      // if(isNum.test(v2)) v2 = parseFloat(v2);
-      if(typeof(v1) === "number" && typeof(v2) === "number"){
-        return v1 - v2;
-      }
-      if(typeof(v1) === "number" && typeof(v2) !== "number"){
-        return v1 - Number(v2);
-      }
-      if(typeof(v1) !== "number" && typeof(v2) === "number"){
-        return Number(v1) - v2;
-      }
+      if(typeof(v1) === "number" && typeof(v2) === "number"){return v1 - v2;};
+      if(typeof(v1) === "number" && typeof(v2) !== "number"){return v1 - Number(v2);};
+      if(typeof(v1) !== "number" && typeof(v2) === "number"){return Number(v1) - v2;};
       
       if(v1 && !v2){
         return 1;
