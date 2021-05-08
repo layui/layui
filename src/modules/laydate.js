d49b898c17798b3d9cfcf186f3229c893f152634
贤心 <3277200+sentsin@users.noreply.github.com>
2021-05-08 21:07:11 +0800

    chore: fixed an error

--- src/modules/laydate.js
+++ src/modules/laydate.js
@@ -1221,7 +1221,7 @@
     ,elem = that.bindElem || options.elem[0];
     
     //静态展现则不作默认赋值
-    if(options.position === 'static') return this;
+    if(options.position === 'static') return that;
     
     value = value || '';
     
@@ -1241,7 +1241,7 @@
       }
     }
     
-    return this;
+    return that;
   };
   
   //预览
