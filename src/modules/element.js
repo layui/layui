fb2d5d455cf871613c051f8916970da4799a0ff3
MonsterUncle <295228902@qq.com>
2022-07-07 14:32:59 +0800

    Update element.js
    tab 删除li click定位不准，加上判断tabName

--- src/modules/element.js
+++ src/modules/element.js
@@ -131,9 +131,9 @@ layui.define('jquery', function(exports){
       ,filter = parents.attr('lay-filter');
       
       if(li.hasClass(THIS)){
-        if(li.next()[0]){
+        if (li.next()[0] && li.next()[0].tagName === 'LI'){
           call.tabClick.call(li.next()[0], null, index + 1);
-        } else if(li.prev()[0]){
+          } else if (li.prev()[0] && li.prev()[0].tagName === 'LI'){
           call.tabClick.call(li.prev()[0], null, index - 1);
         }
       }
