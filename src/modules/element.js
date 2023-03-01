419fc8da27b3c6b98c35d37634c1449eef16755f
贤心 <3277200+sentsim@users.noreply.github.com>
2023-03-02 00:11:49 +0800

    优化 `tab` 中的获取下标的逻辑

--- src/modules/element.js
+++ src/modules/element.js
@@ -113,14 +113,16 @@ layui.define('jquery', function(exports){
     //Tab 点击
     tabClick: function(e, index, liElem, options){
       options = options || {};
-      var othis = liElem || $(this)
-      ,index = index || othis.parent().children('li').index(othis)
-      ,parents = options.headerElem ? othis.parent() : othis.parents('.layui-tab').eq(0)
-      ,item = options.bodyElem ? $(options.bodyElem) : parents.children('.layui-tab-content').children('.layui-tab-item')
-      ,elemA = othis.find('a')
-      ,isJump = elemA.attr('href') !== 'javascript:;' && elemA.attr('target') === '_blank' //是否存在跳转
-      ,unselect = typeof othis.attr('lay-unselect') === 'string' //是否禁用选中
-      ,filter = parents.attr('lay-filter');
+      index = index === undefined 
+        ? othis.parent().children('li').index(othis)
+      : index;
+      var othis = liElem || $(this);
+      var parents = options.headerElem ? othis.parent() : othis.parents('.layui-tab').eq(0);
+      var item = options.bodyElem ? $(options.bodyElem) : parents.children('.layui-tab-content').children('.layui-tab-item');
+      var elemA = othis.find('a');
+      var isJump = elemA.attr('href') !== 'javascript:;' && elemA.attr('target') === '_blank'; //是否存在跳转
+      var unselect = typeof othis.attr('lay-unselect') === 'string'; //是否禁用选中
+      var filter = parents.attr('lay-filter');
       
       //执行切换
       if(!(isJump || unselect)){
@@ -177,8 +179,8 @@ layui.define('jquery', function(exports){
           call.hideTabMore(true)
         }
         
-        //允许关闭
-        if(othis.attr('lay-allowClose')){
+        // 开启关闭图标
+        if(othis.attr('lay-allowclose')){
           title.find('li').each(function(){
             var li = $(this);
             if(!li.find('.'+CLOSE)[0]){
