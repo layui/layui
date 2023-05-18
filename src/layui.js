a884182ed284455e20a7a262a28653051fd0703b
贤心 <3277200+sentsim@users.noreply.github.com>
2023-05-18 13:24:53 +0800

    微调 `layui.link()' 方法中的代码段落

--- src/layui.js
+++ src/layui.js
@@ -268,12 +268,13 @@
     if(typeof fn === 'string') cssname = fn;
     
     var app = (cssname || href).replace(/\.|\//g, '');
-    var id = link.id = 'layuicss-'+ app;
+    var id = 'layuicss-'+ app;
     var STAUTS_NAME = 'creating';
     var timeout = 0;
     
-    link.rel = 'stylesheet';
     link.href = href + (config.debug ? '?v='+new Date().getTime() : '');
+    link.rel = 'stylesheet';
+    link.id = id;
     link.media = 'all';
     
     if(!doc.getElementById(id)){
