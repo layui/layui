b241fceda147d44868af4003822d4443ad07504c
凌天翔龙 <15810208908@qq.com>
2024-05-29 16:13:30 +0800

    优化复选框filter命名方式
    优化复选框filter命名方式

--- src/modules/checkbox.js
+++ src/modules/checkbox.js
@@ -4,31 +4,31 @@
  */
 
 layui.define(['lay', 'layer', 'util', 'form'], function (exports) {
-    "use strict";
+  "use strict";
 
-    function CheckBox() {
+  function CheckBox() {
 
+  }
+  CheckBox.prototype.render = function (options) {
+    options.name = options.name || Math.round();
+    var wrap = $(options.elem).attr("lay-filter", "checkbox_" + options.name), html = [];
+    $.each(options.data || [], function () {
+      html.push('<input type="checkbox" title="' + (this[options.textField] || this.title || "") + '" name="' + options.name + '"' + (this.checked ? " checked" : "") + (this.disabled ? " disabled" : "") + (options.indeterminate ? " indeterminate" : "") + ' value="' + (this[options.valueField] || this.value || this.id || "") + '"' + (this.skin ? ' class="lay-' + this.skin + '"' : "") + ' />');
+    });
+    wrap.html(html.join(''));
+    var vals = options.vals;
+    if (vals) {
+      wrap.find('input').val(vals.constructor == String ? vals.split(",") : vals);
     }
-    CheckBox.prototype.render = function (options) {
-        var wrap = $(options.elem).attr("lay-filter", "fdsafsdafsda"), html = [];
-        options.name = options.name || Math.round();
-        $.each(options.data || [], function () {
-            html.push('<input type="checkbox" title="' + (this[options.textField] || this.title || "") + '" name="' + options.name + '"' + (this.checked ? " checked" : "") + (this.disabled ? " disabled" : "") + (options.indeterminate ? " indeterminate" : "") + ' value="' + (this[options.valueField] || this.value || this.id || "") + '"' + (this.skin ? ' class="lay-' + this.skin + '"' : "") + ' />');
-        });
-        wrap.html(html.join(''));
-        var vals = options.vals;
-        if (vals) {
-            wrap.find('input').val(vals.constructor == String ? vals.split(",") : vals);
-        }
-        layui.form.render("checkbox", "fdsafsdafsda");
-    }
+    layui.form.render("checkbox", "checkbox_" + options.name);
+  }
 
-    var checkbox = {};
-    checkbox.render = function (options) {
-        return new CheckBox().render(options);
-    }
+  var checkbox = {};
+  checkbox.render = function (options) {
+    return new CheckBox().render(options);
+  }
 
-    exports("checkbox", checkbox);
+  exports("checkbox", checkbox);
 });
 
 
