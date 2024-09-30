eace40f0cbc188850210499e011295ce24d9daeb
贤心 <3277200+sentsim@users.noreply.github.com>
2024-09-30 17:33:00 +0800

    fix(layer): 修复 layer.path 潜在的缺陷 (#2257)
    * fix(layer): 修复 layer.path 潜在的缺陷
    
    * chore(layer): update

--- src/modules/layer.js
+++ src/modules/layer.js
@@ -11,8 +11,8 @@ var $;
 var win;
 var ready = {
   getPath: function(){
-    var jsPath = document.currentScript ? document.currentScript.src : function(){
-      var js = document.scripts;
+    var jsPath = (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT') ? document.currentScript.src : function(){
+      var js = document.getElementsByTagName('script');
       var last = js.length - 1;
       var src;
       for(var i = last; i > 0; i--){
