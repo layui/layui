02d1b75cb0ceef0fbd361994c4174dce67fe05ee
贤心 <3277200+sentsim@users.noreply.github.com>
2025-03-10 16:52:05 +0800

    build: 更正 component 模块打包顺序

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ const config = {
     {pkg: pkg, js: ';'}
   ],
   // 全部模块
-  modules: 'lay,component,laytpl,laypage,laydate,jquery,layer,util,dropdown,slider,colorpicker,element,upload,form,table,treeTable,tabs,tree,transfer,carousel,rate,flow,code'
+  modules: 'lay,laytpl,laypage,laydate,jquery,component,layer,util,dropdown,slider,colorpicker,element,upload,form,table,treeTable,tabs,tree,transfer,carousel,rate,flow,code'
 };
 
 // 获取参数
