5a6ce66d0c64f388085a395e5b12f4b384602b12
贤心 <3277200+sentsim@users.noreply.github.com>
2025-04-11 21:49:59 +0800

    fix(tabs): 修复 box-sizing 对主体区域中其他组件的样式影响

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -553,7 +553,7 @@ layui.define('component', function(exports) {
 
     // 滚动结构
     var rollElem = {
-      elem: $('<div class="'+ CLASS_SCROLL +' layui-unselect"></div>'),
+      elem: $('<div class="'+ CLASS_SCROLL +' layui-border-box layui-unselect"></div>'),
       bar: $([
         '<div class="'+ CLASS_BAR +'">',
           '<i class="layui-icon '+ CLASS_BAR_ICON[0] +'" lay-mode="prev"></i>',
