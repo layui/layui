26989d58303f927efcb6b20a6f4bbc1e348573bb
贤心 <3277200+sentsim@users.noreply.github.com>
2025-05-26 22:51:44 +0800

    style(laydate): 优化逗号格式

--- src/modules/laydate.js
+++ src/modules/laydate.js
@@ -164,29 +164,29 @@
 
   // 默认配置
   Class.prototype.config = {
-    type: 'date' //控件类型，支持：year/month/date/time/datetime
-    ,range: false //是否开启范围选择，即双控件
-    ,format: 'yyyy-MM-dd' //默认日期格式
-    ,value: null //默认日期，支持传入new Date()，或者符合format参数设定的日期格式字符
-    ,isInitValue: true //用于控制是否自动向元素填充初始值（需配合 value 参数使用）
-    ,min: '1900-1-1' //有效最小日期，年月日必须用“-”分割，时分秒必须用“:”分割。注意：它并不是遵循 format 设定的格式。
-    ,max: '2099-12-31' //有效最大日期，同上
-    ,trigger: 'click' //呼出控件的事件
-    ,show: false //是否直接显示，如果设置 true，则默认直接显示控件
-    ,showBottom: true //是否显示底部栏
-    ,isPreview: true //是否显示值预览
-    ,btns: ['clear', 'now', 'confirm'] //右下角显示的按钮，会按照数组顺序排列
-    ,lang: 'cn' // 语言，只支持 cn/en，即中文和英文
-    ,theme: 'default' //主题
-    ,position: null //控件定位方式定位, 默认absolute，支持：fixed/absolute/static
-    ,calendar: false //是否开启公历重要节日，仅支持中文版
-    ,mark: {} //日期备注，如重要事件或活动标记
-    ,holidays: null // 标注法定节假日或补假上班
-    ,zIndex: null //控件层叠顺序
-    ,done: null //控件选择完毕后的回调，点击清空/现在/确定也均会触发
-    ,change: null //日期时间改变后的回调
-    ,autoConfirm: true //是否自动确认（日期|年份|月份选择器非range下是否自动确认）
-    ,shade: 0
+    type: 'date', // 控件类型，支持：year/month/date/time/datetime
+    range: false, // 是否开启范围选择，即双控件
+    format: 'yyyy-MM-dd', // 默认日期格式
+    value: null, // 默认日期，支持传入new Date()，或者符合format参数设定的日期格式字符
+    isInitValue: true, // 用于控制是否自动向元素填充初始值（需配合 value 参数使用）
+    min: '1900-1-1', // 有效最小日期，年月日必须用“-”分割，时分秒必须用“:”分割。注意：它并不是遵循 format 设定的格式。
+    max: '2099-12-31', // 有效最大日期，同上
+    trigger: 'click', // 呼出控件的事件
+    show: false, // 是否直接显示，如果设置 true，则默认直接显示控件
+    showBottom: true, // 是否显示底部栏
+    isPreview: true, // 是否显示值预览
+    btns: ['clear', 'now', 'confirm'], // 右下角显示的按钮，会按照数组顺序排列
+    lang: 'cn', // 语言，只支持 cn/en，即中文和英文
+    theme: 'default', // 主题
+    position: null, // 控件定位方式定位, 默认absolute，支持：fixed/absolute/static
+    calendar: false, // 是否开启公历重要节日，仅支持中文版
+    mark: {}, // 日期备注，如重要事件或活动标记
+    holidays: null, // 标注法定节假日或补假上班
+    zIndex: null, // 控件层叠顺序
+    done: null, // 控件选择完毕后的回调，点击清空/现在/确定也均会触发
+    change: null, // 日期时间改变后的回调
+    autoConfirm: true, // 是否自动确认（日期|年份|月份选择器非range下是否自动确认）
+    shade: 0
   };
 
   // 多语言
