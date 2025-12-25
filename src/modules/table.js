dfa3818786d1ba01b745f0922ac12d260374444c
sight <26325820+Sight-wcg@users.noreply.github.com>
2025-12-25 12:14:46 +0800

    update code

--- src/modules/table.js
+++ src/modules/table.js
@@ -1367,9 +1367,13 @@ layui.define(
 
       // 仅由 resizeObserver 触发时生效
       if (entry) {
-        var shouldIgnore = entry.target._lay_lastSize &&
-          Math.abs(entry.target._lay_lastSize.height - entry.contentRect.height) < RESIZE_THRESHOLD &&
-          Math.abs(entry.target._lay_lastSize.width - entry.contentRect.width) < RESIZE_THRESHOLD;
+        var shouldIgnore =
+          entry.target._lay_lastSize &&
+          Math.abs(
+            entry.target._lay_lastSize.height - entry.contentRect.height
+          ) < RESIZE_THRESHOLD &&
+          Math.abs(entry.target._lay_lastSize.width - entry.contentRect.width) <
+            RESIZE_THRESHOLD;
 
         if (shouldIgnore) return;
 
