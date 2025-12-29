c687dc58dbf432307520a08fc2982e6f4dcfbb1e
morning-star <26325820+Sight-wcg@users.noreply.github.com>
2025-12-29 15:00:01 +0800

    fix(flow): 修复图片懒加载异常 (#2965)

--- src/modules/flow.js
+++ src/modules/flow.js
@@ -198,7 +198,7 @@ layui.define(['i18n', 'component'], function (exports) {
                   item.attr('src', src).removeAttr('lay-src');
 
                   /* 当前图片加载就绪后，检测下一个图片是否在当前屏 */
-                  next[0] && render(next);
+                  next[0] && fn(next);
                   index++;
                 },
                 function () {
