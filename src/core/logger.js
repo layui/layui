b7f0af5c80678ddacf7ef61db332fa421b4e4b3c
贤心 <3277200+sentsim@users.noreply.github.com>
2026-04-09 13:10:58 +0800

    feat(core): 新增 logger 模块

---
+++ src/core/logger.js
@@ -0,0 +1,39 @@
+/**
+ * logger
+ */
+
+let warned = Object.create(null);
+
+/**
+ * 控制台日志消息提示
+ * @param {string} message - 消息内容
+ * @param {'warn'|'error'} [level='warn'] - 消息级别
+ */
+export function log(message, level = 'warn') {
+  message = `[Layui ${level}]: ${message}`;
+
+  // 限定消息级别
+  if (!/^(warn|error)$/.test(level.trim())) {
+    level = 'warn';
+  }
+
+  window.console[level](message);
+}
+
+/**
+ * 一次性「日志提示」
+ * @param {...Parameters<typeof log>} args
+ */
+export function logOnce(...args) {
+  const [message] = args;
+
+  if (warned._size > 100) {
+    warned = Object.create(null);
+    warned._size = 0;
+  }
+  if (!warned[message]) {
+    warned[message] = true;
+    warned._size = (warned._size || 0) + 1;
+    log(...args);
+  }
+}
